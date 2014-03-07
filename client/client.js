09ef6690ea3eaeb98156a3a1c1d1b3149d60e8af
Brian Chu <bc@brianchu.com>
2014-03-07 05:38:48 -0800

    Adding todos, remove server init code

--- client/client.js
+++ client/client.js
@@ -1,3 +1,10 @@
 Template.list.todos = function() {
   return Todos.find();
 };
+
+Template.app.events({
+  'click button': function(event) {
+    var taskName = event.target.parentElement.querySelector('input').value;
+    Todos.insert({name: taskName});
+  }
+});
