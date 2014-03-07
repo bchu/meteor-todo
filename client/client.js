261d41dd8f1611ef1ff25752d50fd94379a1c8ef
Brian Chu <bc@brianchu.com>
2014-03-07 05:41:29 -0800

    Upvote with score

--- client/client.js
+++ client/client.js
@@ -5,7 +5,7 @@ Template.list.todos = function() {
 Template.app.events({
   'click button': function(event) {
     var taskName = event.target.parentElement.querySelector('input').value;
-    Todos.insert({name: taskName});
+    Todos.insert({name: taskName, score:0});
   }
 });
 
@@ -13,5 +13,9 @@ Template.list.events({
   'click .delete': function(event) {
     var id = event.target.parentElement.getAttribute('data-id');
     Todos.remove(id);
+  },
+  'click .upvote': function(event) {
+    var id = event.target.parentElement.getAttribute('data-id');
+    Todos.update(id, {$inc: {score: 1}});
   }
 });
