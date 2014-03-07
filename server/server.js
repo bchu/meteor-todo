5b7e02d284e3b6df02c79237898a00e2814f5737
Brian Chu <bc@brianchu.com>
2014-03-07 05:45:52 -0800

    Update validation

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ Meteor.startup(function() {
   Todos.allow({
     'insert': function(userId, entry) {
       return entry.score === 0;
+    },
+    'update': function(userId, entry, fieldNames, modifier) {
+      return modifier.$inc && modifier.$inc.score && modifier.$inc.score == 1;
     }
   });
 });
